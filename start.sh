c40acf0a99192c3a8a88bd1a85f3927655945160
wuhanstudio <wuhanstudio@qq.com>
2021-12-31 15:26:29 +0000

    Add gst-launch

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 # ./app.py | ffmpeg -s 640x480 -f rawvideo -pix_fmt bgr24 -framerate 10 -i - -s 1280x720 -vf crop=w=640:h=360 -r 10 -f v4l2 -vcodec mjpeg -pix_fmt yuvj420p /dev/video0
 ./app.py -c 2 | ffmpeg -s 640x480 -f rawvideo -pix_fmt bgr24 -framerate 10 -i - -s 1280x720 -vf crop=w=640:h=360 -r 10 -f v4l2 -vcodec rawvideo -pix_fmt yuyv422 /dev/video0
+
+# gst-launch-1.0 videotestsrc ! v4l2sink device=/dev/video0
